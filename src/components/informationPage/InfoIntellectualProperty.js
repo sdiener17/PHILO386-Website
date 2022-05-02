463152a73bab9252da51498c5d3489fb1af5d726
Sarah Diener <sdiener@ksu.edu>
2022-05-01 23:10:35 -0500

    Updated info pages

--- src/components/informationPage/InfoIntellectualProperty.js
+++ src/components/informationPage/InfoIntellectualProperty.js
@@ -1,15 +1,38 @@
 import React from "react";
 import styled from "styled-components";
 import TextAndImageCard from "./TextAndImageCard";
+import TextOnlyCard from "./TextOnlyCard";
+import chef1 from "../../images/chef1.jpg";
+import {infoIPText} from "../../data/infoIPText";
+import law1 from "../../images/law1.jpg";
 
 export default function InformationPage(){
     return (
         <PageWrapper>
-            <TextAndImageCard/>
+            <div className="marginTop"/>
+            <h1>History</h1>
+            <TextAndImageCard image={chef1} text={infoIPText.text1} />
+            <h1>Modern Laws Relating to Intellectual Property</h1>
+            <TextAndImageCard image={law1} text={infoIPText.text2}/>
+            <h1>Copyright Law Details</h1>
+            <TextOnlyCard text={infoIPText.text3}/>
+            <div className="marginBottom"/>
         </PageWrapper>
     )
 }
 
 const PageWrapper = styled.nav`
     display:flex;
+    justify-content:center;
+    flex-direction:column;
+    width:100%;
+    padding-top:20px;
+    padding-bottom:60px;
+    //margin-left:40px;
+    .marginTop{
+        margin-bottom:10px;
+    }
+    .marginBottom{
+        margin-bottom:var(--marginBottom);
+    }
 `;
