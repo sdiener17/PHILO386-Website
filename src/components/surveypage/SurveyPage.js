317270de56617427439cc3e38b7bb9b795679640
Sarah Diener <sdiener@ksu.edu>
2022-05-02 18:28:23 -0500

    Worked on survey page

--- src/components/surveypage/SurveyPage.js
+++ src/components/surveypage/SurveyPage.js
@@ -17,6 +17,14 @@ export default function SurveyPage({setIsSurveySubmitted, setUserAnswers}){
     const [radioValueQ1, setRadioValueQ1] = useState(null);
     const [surveyQuestion2, setSurveyQuestion2] = useState("");
     const [radioValueQ2, setRadioValueQ2] = useState(null);
+    const [surveyQuestion3, setSurveyQuestion3] = useState("");
+    const [radioValueQ3, setRadioValueQ3] = useState(null);
+    const [surveyQuestion4, setSurveyQuestion4] = useState("");
+    const [radioValueQ4, setRadioValueQ4] = useState(null);
+    const [surveyQuestion5, setSurveyQuestion5] = useState("");
+    const [radioValueQ5, setRadioValueQ5] = useState(null);
+    const [surveyQuestion6, setSurveyQuestion6] = useState("");
+    const [radioValueQ6, setRadioValueQ6] = useState(null);
 
 
 
@@ -33,6 +41,10 @@ export default function SurveyPage({setIsSurveySubmitted, setUserAnswers}){
             <div className="surveyTitle">Intellectual Property Survey</div>
             <SurveyQuestion questionText={questions[0].questionText} setRadioValue={setRadioValueQ1} nameOfGroup="group1"/>
             <SurveyQuestion questionText={questions[1].questionText} setRadioValue={setRadioValueQ2} nameOfGroup="group2"/>
+            <SurveyQuestion questionText={questions[2].questionText} setRadioValue={setRadioValueQ3} nameOfGroup="group3"/>
+            <SurveyQuestion questionText={questions[3].questionText} setRadioValue={setRadioValueQ4} nameOfGroup="group4"/>
+            <SurveyQuestion questionText={questions[4].questionText} setRadioValue={setRadioValueQ5} nameOfGroup="group5"/>
+            <SurveyQuestion questionText={questions[5].questionText} setRadioValue={setRadioValueQ6} nameOfGroup="group6"/>
             <button className="submitButton" onClick={handleSubmitClick}>Submit</button>
             <div className="marginBottom"/>
         </PageWrapper>
--- src/data/questions.js
+++ src/data/questions.js
@@ -1,7 +1,7 @@
 export const questions = [
     {
         questionNumber:1,
-        questionText: "Is this a question?",
+        questionText: "Is it ethical to download copyrighted music from the internet without the creators consent? (Pirate Music)",
         questionData: [
             { label: "Yes", students: 400 },
             { label: "No", students: 700 },
@@ -9,10 +9,42 @@ export const questions = [
     },
     {
         questionNumber:2,
-        questionText:"This is a really very long question. Of that I am quite sure. So I just wanted to ask you, is blue your most favorite color?",
+        questionText: "Is it okay to download a copyrighted video game from the internet if the creator has discontinued production of the original game? (Pirate Video Games)",
         questionData:[
             {label:"Yes", students: 100},
             {label:"No", students:950}
+        ]
+    },
+    {
+        questionNumber: 3,
+        questionText: "Is it okay to download a copyrighted video game if the creator has discontinued production of the original game but still is producing a sequel?",
+        questionData:[
+            {label:"Yes", students: 9},
+            {label:"No", students: 5}
+        ]
+    },
+    {
+        questionNumber: 4,
+        questionText:"Is it okay to download a copyrighted video game if the creator has discontinued production of the original game but still is producing a sequel?",
+        questionData:[
+            {label:"Yes", students:3},
+            {label:"No", students: 13}
+        ]
+    },
+    {
+        questionNumber:5,
+        questionText:"Is it ethical to download a copyrighted video game if the creator has discontinued production on the original game but is producing the game on a different console?",
+        questionData:[
+            {label:"Yes", students: 6},
+            {label: "No", students: 7}
+        ]
+    },
+    {
+        questionNumber:6,
+        questionText:"If you purchase a piece of software and the creator updates it and removes/changes functionality and forces you to update are you entitled to a refund?",
+        questionData:[
+            {label:"Yes", students: 14},
+            {label:"No", students: 1}
         ]
     }
 ]
